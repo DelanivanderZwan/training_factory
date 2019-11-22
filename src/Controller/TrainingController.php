1db3b517b7d304693bae4cc52026b67a003cc927
DelanivanderZwan <302372741@student.rocmondriaan.nl>
2019-11-22 10:10:00 +0100

    Added all the twig pages and made the homepage start

--- src/Controller/TrainingController.php
+++ src/Controller/TrainingController.php
@@ -4,13 +4,17 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 
 class TrainingController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('OMG! My firt page already! Wooo!');
     }
+
 }
